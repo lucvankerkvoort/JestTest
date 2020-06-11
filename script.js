0957bf90e835929bde7214c5f1364e3be55c5388
Luc van Kerkvoort <luc.van.kerkvoort@gmail.com>
2020-06-11 15:54:28 -0700

    added the googleSearch function to the test script

--- script.js
+++ script.js
@@ -1,17 +1,19 @@
 const googleDatabase = [
-    "cats.com",
-    "souprecipes.com",
-    "whatever.com",
-    "catcrazy.com",
-    "crazycats.com",
-    "catspics.com"
+  "cats.com",
+  "souprecipes.com",
+  "whatever.com",
+  "catcrazy.com",
+  "crazycats.com",
+  "catspics.com",
 ];
 
-const googleSearch = (searchInput) =>{
-    const matches = googleDatabase.filter(website => {
-        return website.includes(searchInput)
-    })
-    return matches.length > 3 ? matches.slice(0,3) : matches
-}
+const googleSearch = (searchInput) => {
+  const matches = googleDatabase.filter((website) => {
+    return website.includes(searchInput);
+  });
+  return matches.length > 3 ? matches.slice(0, 3) : matches;
+};
 
-console.log(googleSearch("cat"));
+// console.log(googleSearch("cat"));
+
+module.exports = googleSearch;
