7f8bd0209abcfddda02746235fd05b558d956cff
Luc van Kerkvoort <luc.van.kerkvoort@gmail.com>
2020-06-11 16:12:06 -0700

    added 2 additional tests

--- script.js
+++ script.js
@@ -7,13 +7,13 @@ const googleDatabase = [
   "catspics.com",
 ];
 
-const googleSearch = (searchInput) => {
-  const matches = googleDatabase.filter((website) => {
+const googleSearch = (searchInput, db) => {
+  const matches = db.filter((website) => {
     return website.includes(searchInput);
   });
   return matches.length > 3 ? matches.slice(0, 3) : matches;
 };
 
-// console.log(googleSearch("cat"));
+// console.log(googleSearch("cat", googleDatabase));
 
 module.exports = googleSearch;
--- script.test.js
+++ script.test.js
@@ -1,5 +1,21 @@
 const googleSearch = require("./script");
 
-it("this is a test", () => {
-  console.log(googleSearch);
+const dbMock = ["dog.com", "cheesepuff.com", "disney.com", "dogpictures.com"];
+it("this is a silly test", () => {
+  expect("hello").toBe("hello");
+  //   googleSearch("testtest", dbMock);
+});
+
+it("is searching google", () => {
+  expect(googleSearch("testtest", dbMock)).toEqual([]);
+  expect(googleSearch("dog", dbMock)).toEqual(["dog.com", "dogpictures.com"]);
+});
+
+it("work with undefined and null entries", () => {
+  expect(googleSearch(undefined, dbMock)).toEqual([]);
+  expect(googleSearch(null, dbMock)).toEqual([]);
+});
+
+it("does not return more than 3 matches", () => {
+  expect(googleSearch(".com", dbMock).length).toEqual(3);
 });
